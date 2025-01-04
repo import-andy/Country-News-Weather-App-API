d010b476902e0805769ed23d8d9370c91d8f6319
Andrii_Desktop <andrii.skvortsov.ua@gmail.com>
2025-01-04 18:53:11 +0000

    fixed home.page.ts ans .htms (website launches now)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,17 +1,27 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonicModule, FormsModule]  // Ensure IonicModule is imported here
+  imports: [IonicModule, CommonModule, FormsModule]
 })
 export class HomePage {
     studentNumber = 'G00438837';
     countryName = '';
+
+    constructor(private navCtrl: NavController) {}
+
+    searchCountries() {  //  Method is correctly defined here
+        if (this.countryName.trim()) {
+            this.navCtrl.navigateForward(`/countries/${this.countryName}`);
+        } else {
+            alert('Please enter a valid country name.');
+        }
+    }
 }
